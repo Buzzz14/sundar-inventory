61e4ddac3e7fac5c52afda762473b3fae896004d
Buzzz14 <sushanbajracharya14@gmail.com>
2025-09-15 14:24:49 +0545

    feat(categories): complete CRUD operations

--- backend/src/controllers/categoriesController.js
+++ backend/src/controllers/categoriesController.js
@@ -1,18 +1,71 @@
 import Category from "../models/categoryModel.js";
 
 export const getCategories = async (req, res) => {
-  const categories = await Category.find();
-  res.json(categories);
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    res.status(500).json({ message: "Server error. Please try again." });
+  }
 };
 
 export const createCategory = async (req, res) => {
   try {
     const { name, description } = req.body;
+
+    const categoryExists = await Category.findOne({ name });
+    if (categoryExists) {
+      return res.status(400).json({ message: "Category already exists" });
+    }
+
     const category = new Category({ name, description });
     await category.save();
-    res.status(201).json({ message: "Category created successfully"});
+
+    res.status(201).json({ message: "Category created successfully" });
   } catch (error) {
     console.error("Error creating category:", error);
+    res.status(500).json({ message: "Server error. Please try again." });
+  }
+};
+
+export const updateCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      id,
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedCategory) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    res.json(updatedCategory);
+  } catch (error) {
+    console.error("Error updating category:", error);
+    res.status(500).json({ message: "Server error. Please try again." });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(404).json({
+        message: "Category not found",
+      });
+    }
+
+    await category.deleteOne();
+    res.json({ message: "Category deleted successfully" });
+  } catch (err) {
+    console.error("Error deleting category:", err);
     res.status(500).json({ message: "Server error. Please try again." });
   }
 };
